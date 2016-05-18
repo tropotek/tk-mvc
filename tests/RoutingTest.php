0ac2e130e3face60f699f734611218420aecb26f
tropotek <info@tropotek.com>
2016-05-18 11:50:45 +1000

    -

---
+++ tests/RoutingTest.php
@@ -0,0 +1,49 @@
+<?php
+namespace tests;
+
+
+
+/**
+ * Class DispatcherTest
+ *
+ * @author Michael Mifsud <info@tropotek.com>
+ * @link http://www.tropotek.com/
+ * @license Copyright 2016 Michael Mifsud
+ */
+class RoutingTest extends \PHPUnit_Framework_TestCase
+{
+    
+
+
+    public function __construct()
+    {
+        parent::__construct('Routing Test');
+    }
+
+    public function setUp()
+    {
+        
+
+    }
+
+    public function tearDown()
+    {
+
+    }
+
+    
+    
+    public function testRoutCollection()
+    {
+        
+    }
+
+
+    public function testRoute()
+    {
+
+    }
+    
+    
+}
+
