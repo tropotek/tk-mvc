07d720e27bb8ddba718a45b8b414457f7d83d914
tropotek <info@tropotek.com>
2017-03-09 14:19:37 +1100

    Minor Code Updates - dev

--- Tk/Listener/ShutdownHandler.php
+++ Tk/Listener/ShutdownHandler.php
@@ -67,9 +67,9 @@ class ShutdownHandler implements SubscriberInterface
             //$this->out('------------------------------------------------');
             $this->out('Response Headers:');
             $this->out('  HTTP Code: ' . http_response_code() . ' ');
-            foreach (headers_list() as $line) {
-                $this->out('  ' . $line);
-            }
+//            foreach (headers_list() as $line) {
+//                $this->out('  ' . $line);
+//            }
             //$this->log->debug($this->toString());
             $this->out('------------------------------------------------' . \PHP_EOL);
         }
