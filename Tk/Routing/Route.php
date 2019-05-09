b8ce6e87d804e182364fc98e6fccd34a02d86082
Tropotek <info@tropotek.com>
2019-05-10 08:09:48 +1000

    ~Auto: Commit

---
+++ Tk/Routing/Route.php
@@ -0,0 +1,30 @@
+<?php
+namespace Tk\Routing;
+
+
+/**
+ * @author Michael Mifsud <info@tropotek.com>
+ * @see http://www.tropotek.com/
+ * @license Copyright 2016 Michael Mifsud
+ * @notes Adapted from Symfony
+ * @deprecated Use: \Symfony\Component\Routing\Route() This will be removed once the symfony Routing lib is used by all projects
+ */
+class Route extends \Symfony\Component\Routing\Route
+{
+
+    /**
+     * Left for compatability
+     * will be deleted once all project routes.php files are updated
+     *
+     * @param string $path
+     * @param object|callable|string $controller A string, callable or object
+     * @param array $attributes
+     * @param array $validMethods
+     * @deprecated Use: new \Symfony\Component\Routing\Route()
+     */
+    public function __construct($path, $controller, $attributes = array(), $validMethods = array('GET','POST','HEAD'))
+    {
+        $defaults = array_merge(array('_controller' => $controller), $attributes);
+        parent::__construct($path, $defaults, [], [], '', [], $validMethods);
+    }
+}
