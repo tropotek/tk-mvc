ffdd5ee99942d084872b0a76ea67858eb6b75250
mifsudm <mifsudm@unimelb.edu.au>
2019-07-30 09:39:17 +1000

    ~Auto: Commit

--- Tk/Controller/Iface.php
+++ Tk/Controller/Iface.php
@@ -146,7 +146,7 @@ abstract class Iface extends \Dom\Renderer\Renderer implements \Dom\Renderer\Dis
     /**
      * @return \Tk\Session
      */
-    public function getTrack()
+    public function getDomain()
     {
         return $this->getConfig()->getSession();
     }
