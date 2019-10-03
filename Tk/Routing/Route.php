05a9388d9830f209ca521fc47b37e0a8ae43f159
mifsudm <mifsudm@unimelb.edu.au>
2019-10-03 12:01:48 +1000

    Update Tk\Routing\Route constructor to be compatable with sysmfony Route object

--- Tk/Routing/Route.php
+++ Tk/Routing/Route.php
@@ -6,11 +6,33 @@ namespace Tk\Routing;
  * @author Michael Mifsud <info@tropotek.com>
  * @see http://www.tropotek.com/
  * @license Copyright 2016 Michael Mifsud
- * @notes Adapted from Symfony
- * @deprecated Use: \Symfony\Component\Routing\Route() This will be removed once the symfony Routing lib is used by all projects
  */
 class Route extends \Symfony\Component\Routing\Route
 {
+
+    /**
+     * Route constructor.
+     *
+     * @param string $path
+     * @param array|string $defaults  (if sting then use Tk lib compatability untill we change all route calls)
+     * @param array $requirements
+     * @param array $options
+     * @param string|null $host
+     * @param array $schemes
+     * @param array $methods
+     * @param string|null $condition
+     */
+    public function __construct(string $path, $defaults = [], array $requirements = [], array $options = [], ?string $host = '', $schemes = [], $methods = [], ?string $condition = '')
+    {
+        if (!is_array($defaults)) {     // Use this to fix \Tk\Routing\Route params compatability
+            $defaults = array_merge(array('_controller' => $defaults), $requirements);
+            $requirements = [];
+            //$methods = count($options) ? $options : array('GET','POST','HEAD');
+            $methods = $options;
+            $options = [];
+        }
+        parent::__construct($path, $defaults, $requirements, $options, $host, $schemes, $methods, $condition);
+    }
 
     /**
      * Left for compatability
@@ -22,9 +44,27 @@ class Route extends \Symfony\Component\Routing\Route
      * @param array $validMethods
      * @deprecated Use: new \Symfony\Component\Routing\Route()
      */
-    public function __construct($path, $controller, $attributes = array(), $validMethods = array('GET','POST','HEAD'))
+//    public function __construct($path, $controller, $attributes = array(), $validMethods = array('GET','POST','HEAD'))
+//    {
+//        $defaults = array_merge(array('_controller' => $controller), $attributes);
+//        parent::__construct($path, $defaults, [], [], '', [], $validMethods);
+//    }
+
+
+    /**
+     * @param string $path
+     * @param array|string $defaults  (if sting then use Tk lib compatability untill we change all route calls)
+     * @param array $requirements
+     * @param array $options
+     * @param string|null $host
+     * @param array $schemes
+     * @param array $methods
+     * @param string|null $condition
+     * @return \Symfony\Component\Routing\Route
+     */
+    public static function create(string $path, $defaults = [], array $requirements = [], array $options = [], ?string $host = '', $schemes = [], $methods = [], ?string $condition = '')
     {
-        $defaults = array_merge(array('_controller' => $controller), $attributes);
-        parent::__construct($path, $defaults, [], [], '', [], $validMethods);
+        return new self($path, $defaults, $requirements, $options, $host, $schemes, $methods, $condition);
     }
+
 }
