0a3bcd4c2d41a4e7f0b779338a9dcc29ac74009c
tropotek <info@tropotek.com>
2020-05-28 19:18:32 +1000

    ~Auto: Commit

--- Tk/Console/Console.php
+++ Tk/Console/Console.php
@@ -64,9 +64,9 @@ abstract class Console extends Command
      */
     protected function initialize(InputInterface $input, OutputInterface $output)
     {
-//        if (!\Tk\FileLocker::lockFile($this->locFile)) {
-//            //throw new \Tk\NoticeException('Instance alqready executing. Aborting.', self::ERROR_CODE_INSTANCE_EXISTS);
-//        }
+        if (!\Tk\FileLocker::lockFile($this->locFile)) {
+            //throw new \Tk\NoticeException('Instance alqready executing. Aborting.', self::ERROR_CODE_INSTANCE_EXISTS);
+        }
     }
 
     /**
@@ -76,10 +76,10 @@ abstract class Console extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if (!\Tk\FileLocker::lockFile($this->locFile)) {
-            \Tk\Log::debug('Instance already executing. Aborting.');
-            return 0;
-        }
+//        if (!\Tk\FileLocker::lockFile($this->locFile)) {
+//            \Tk\Log::debug('Instance already executing. Aborting.');
+//            return 0;
+//        }
 
         $this->setInput($input);
         $this->setOutput($output);
